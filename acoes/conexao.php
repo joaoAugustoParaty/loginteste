175b01455bea79a2c27f4c9987737ea218e99a65
João Augusto de Oliveira <joaoaugustowebdev@gmail.com>
2021-10-18 22:39:24 -0300

    Dia 1

--- acoes/conexao.php
+++ acoes/conexao.php
@@ -1,15 +1,14 @@
 <?php
-$server ="localhost";
-$usuario ="root";
-$senha="";
-$banco ="videoaula";
+    $server = "127.0.0.1";
+    $usuario = "root";
+    $senha = "";
+    $banco = "videoaula";
 
-try{
-$conexao = new PDO("mysql:host=$server;dbname=$banco", $usuario, $senha);
-$conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}catch(PDOException $erro){
-    echo "Ocorreu um erro de conexao: {$erro->getMessage()}";
-    $conexao = null;
-
-}
+    try{
+        $conexao = new PDO("mysql:host=$server;dbname=$banco", $usuario, $senha);
+        $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }catch(PDOException $erro){
+        echo "Ocorreu um erro de conexao: {$erro->getMessage()}";
+        $conexao = null;
+    }
 ?>
